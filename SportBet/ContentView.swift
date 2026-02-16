0336d8878a689704872b607e5837cbe8114061fe
keshew <artyom.korotckov2017@yandex.ru>
2026-02-16 14:09:22 +0500

    Initial Commit

---
+++ SportBet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SportBet
+//
+//  Created by Артём Коротков on 16.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SportBet/SportBetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportBetApp.swift
+//  SportBet
+//
+//  Created by Артём Коротков on 16.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct SportBetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
